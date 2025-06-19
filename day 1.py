cec6331551c2dfb56fb6be3d14d29250b4b1d383
Aniket Shinde <161941144+aniket-01-bot@users.noreply.github.com>
2025-06-19 16:17:45 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,73 @@
+# #que 1
+
+# a=int (input("enter the 1st no:"))
+# b=int (input("enter the 2nd no:"))
+# sum=a+b
+# print (f"sum={sum}")
+# #que 2............
+# side=int(input("enter the side:"))
+# area=side*side
+# print(f"area is{area}")
+# #q3........
+# num1=float(input("enter 1st no:"))
+
+# num2=float(input("enter 2nd no:"))
+# avg=(num1+  num2/2)
+# print(f"Average of two number is{avg}")
+#q4 ........
+# a=int (input("enter the 1st no:"))
+# b=int (input("enter the 2nd no:"))
+# if(a>=b):
+#     print("true")
+# else:
+#     print("false")
+#     print(a>=b)
+#print lenth and name...........
+# name =input("enter the name :")
+# print(f"your name is:{name}")
+
+# print(f"length of name:{len(name)}")  
+#check the 1st and last is same..............
+# str=str(input("enter the name:      "))
+# if str[0]==str[-1]:
+#     print("true")
+# else:
+#     print("false")
+#check it is palindrome'''''''''''''
+# input_str = input("Enter a string or number: ")
+
+# reversed_str = input_str[::-1]
+
+# print("Reversed:", reversed_str)
+
+# if input_str == reversed_str:
+#     print("True - It is a palindrome.")
+# else:
+#     print("False - It is not a palindrome.")
+#check it is even or odd.......
+# num = int(input("Enter a number: "))
+
+# if num % 2 == 0:
+#     print(f"{num} is even.")
+# else:
+#     print(f"{num} is odd.")
+#gretst of 3 nos......................
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# num3 = float(input("Enter third number: "))
+
+# if num1 >= num2 and num1 >= num3:
+#     greatest = num1
+# elif num2 >= num1 and num2 >= num3:
+#     greatest = num2
+# else:
+#     greatest = num3
+
+# print("Greatest number is:", greatest)
+#check no is mul of 7 ''''''''''''''''''
+num = int(input("Enter a number: "))
+
+if num % 7 == 0:
+    print(f"{num} is a multiple of 7.")
+else:
+    print(f"{num} is not a multiple of 7.")
